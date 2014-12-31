cd0efbccb85acc93d1a26157d7aff96b6f3f603f
tamarochka <tamara.zenkevich@gmail.com>
2014-12-30 19:06:16 -0500

    fixed login/logout in the layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,11 @@
 class SearchesController < ApplicationController
 
   def search
-    @tops_search = Top.search(:name_like => params[:search])
+    @tops_search = Top.where(:name_like => params[:search])
+  end
+
+  def new
+
   end
 
 end
