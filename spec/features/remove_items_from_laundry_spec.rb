8f6fc09130d92d06e301b6191aac13a5c26877a0
tamarochka <tamara.zenkevich@gmail.com>
2015-01-22 11:25:10 -0500

    added test for removing item from laundry

--- spec/features/remove_items_from_laundry_spec.rb
+++ spec/features/remove_items_from_laundry_spec.rb
@@ -11,7 +11,7 @@ feature 'User mark clothes as clean', %Q{
       login_as(@user)
     end
 
-    scenario 'clothing used in outfit shows up in laundry' do
+    scenario 'removing clothing from laundry bag' do
 
       bottom = FactoryGirl.create(:clothing, clothing_type: "Top", clothing_style: "Sweater", user: @user)
       top = FactoryGirl.create(:clothing, clothing_type: "Bottom", clothing_style: "Jeans", user: @user)
@@ -19,22 +19,10 @@ feature 'User mark clothes as clean', %Q{
 
       visit '/laundry'
 
-      expect(page).to have_content top.clothing_style
-      expect(page).to have_content bottom.clothing_style
-
-    end
-
-
-    scenario 'empty laundry bag' do
-
-      bottom = FactoryGirl.create(:clothing, clothing_type: "Top", clothing_style: "Sweater", user: @user)
-      top = FactoryGirl.create(:clothing, clothing_type: "Bottom", clothing_style: "Jeans", user: @user)
-
-      visit '/laundry'
+      first('.edit_clothing').click_on('Mark as clean')
 
       expect(page).not_to have_content top.clothing_style
-      expect(page).not_to have_content bottom.clothing_style
-      expect(page).to have_content "There are no clothing in you laundry bag"
+      expect(page).to have_content bottom.clothing_style
 
     end
 
